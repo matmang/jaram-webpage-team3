06edc1fccbb2df499c598983c937962595d671c9
matmang <64837797+matmang@users.noreply.github.com>
2021-07-28 13:41:09 +0900

    Create LandingPage.js

---
+++ src/components/LandingPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <header>
+      <div class="titles">
+        <h1 class="homepage_title" style={{position: "center center"}}>
+          JARAM
+        </h1>
+        <h1 class="homepage_subtitle" style={{position: "center center"}}>
+          안녕하세요. 한양대학교 ERICA 캠퍼스 소프트웨어융합대학 전공학회
+          자람입니다.
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default LandingPage;
